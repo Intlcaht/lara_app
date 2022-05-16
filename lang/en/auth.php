09db9d061b5ab56da854e359f3357064c9db4b1e
Peter Vincent <39063789+dev4vin@users.noreply.github.com>
2022-05-16 11:23:34 +0300

    inital commit

---
+++ lang/en/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'First_Name' => 'First Name',
+    'Last_Name' => 'Last Name',
+    'Phone_Number' => 'Phone Number',
+    'Initials' => 'Initials'
+];
